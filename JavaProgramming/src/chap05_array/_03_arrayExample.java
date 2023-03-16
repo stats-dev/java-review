82d0ea40c51fd538af43e4d6eb0e97bcd5e15fa3
stats-dev <hohun94@naver.com>
2023-03-16 11:51:02 +0900

    array java updates

--- JavaProgramming/src/chap05_array/_03_arrayExample.java
+++ JavaProgramming/src/chap05_array/_03_arrayExample.java
@@ -8,16 +8,20 @@ public class _03_arrayExample {
 		char[] convertArr = new char[6];
 		
 
-//		int index = 0;
+		//while로도 된다!
+		int index = 0;
 		
-//		while(index < 5) {
-//			if(chArr[index] >= 'A' && chArr[index] <= 'Z') {
-//				convertArr[index++] = (char) (chArr[index++] + 32);
-//			} else {
-//				convertArr[index++] = (char) (chArr[index++] - 32);
-//			}
-//			
-//		}
+		while(index < chArr.length) {
+			if(chArr[index] >= 'A' && chArr[index] <= 'Z') {
+				convertArr[index] = (char) (chArr[index] + 32);
+			} else {
+				convertArr[index] = (char) (chArr[index] - 32);
+			}
+			index++;
+		}
+		
+		System.out.println(chArr);
+		System.out.println(convertArr);
 		
 		
 		//소문자는 대문자로, 대문자는 소문자로 변경 후 convertArr에 저장
