bcc1e345025d48a06f2afa385952ccf25add22ca
stats-dev <hohun94@naver.com>
2023-04-06 15:15:10 +0900

    lambda and character call updates

--- JavaProgramming/src/chap20_lambda/_10_lambdaEx1.java
+++ JavaProgramming/src/chap20_lambda/_10_lambdaEx1.java
@@ -1,7 +1,9 @@
 package chap20_lambda;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import chap20_lambda.clazz.UserCharString;
@@ -12,21 +14,65 @@ public class _10_lambdaEx1 {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
+		
 		List<UserCharString> ucsList = new ArrayList<UserCharString>();
+
+		/*List<Map<String, String>> mapList = new ArrayList<Map<String, String>>();
+
+		for(int i = 0; i < 5; i++) {
+			Map<String, String> map = new HashMap<String, String>(); //Map 추상클래스이므로 구현된 친구를 찾아가야 한다.
+			
+			map.put(String.valueOf(i), String.valueOf(i));
+			
+			mapList.add(map);
+		}
+		System.out.println(mapList);
+		*/
+		
+//		UserCharString ucs = new UserCharString(); //객체는 값이다. 생성자를 통해 값을 주어야 한다.
 		
 		//10개 입력
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 3; i++) {
 			System.out.println("문자열을 입력하세요.");
-			String str = sc.nextLine();
-			ucsList.add(new UserCharString(str));
+			ucsList.add(new UserCharString(sc.nextLine()));
+//			String userInput = sc.nextLine();
+//			ucsList.add(new UserCharString(sc.nextLine()));
+			
 		}
 		
 		
 		
 		System.out.println("찾을 문자를 입력하세요.");
-		char findCh = sc.nextLine().charAt(0);
+		char findCh = sc.nextLine().charAt(0); //
 		
-		UserCharString ucs = new UserCharString();
+		for(UserCharString u : ucsList) {
+			u.changeStr(findCh, (char c, String s) -> {
+				String returnStr = "";
+				for(int i = 0; i < s.length(); i++) {
+					if(s.charAt(i) == c) {
+						returnStr = s;
+					}
+				}
+				return returnStr;
+			});
+		}
+		
+		
+//		UserCharString ucs = new UserCharString(null);
+//		ucsList.stream().map(c -> c.changeStr(findCh, c));
+//		
+//			System.out.println(c.toString());));
+//		
+//		ucs.changeStr(findCh, (ch, str) -> {
+//			if(str.contains(String.valueOf(ch))) {
+//				System.out.println(str);
+//			}
+//			
+//			
+//		});
+		
+		System.out.println(ucsList);
+
 		
 		
 		sc.close();
--- JavaProgramming/src/chap20_lambda/clazz/UserCharString.java
+++ JavaProgramming/src/chap20_lambda/clazz/UserCharString.java
@@ -5,12 +5,7 @@ public class UserCharString {
 
 	private String str;
 
-	//생성자
-	public UserCharString(String str) {
-		this.str = str;
-	}
 		
-	
 	public String getStr() {
 		return str;
 	}
@@ -19,18 +14,23 @@ public class UserCharString {
 		this.str = str;
 	}
 	
+	//생성자
+	public UserCharString(String str) {
+		this.str = str;
+	}
 	
+	
+	//함수형 인터페이스를 매개변수로 갖는 메소드가 호출되는 부분에서
+	//함수형 인터페이스를 람다식으로 구현한다.
+	public void changeStr(char ch, UserChar uc) {
+		this.str = uc.findUserChar(ch, this.str);
+		
+	}
 	
 	@Override
 	public String toString() {
 		return "UserCharString [str=" + str + "]";
 	}
-
-
-	public void changeStr(char ch, UserChar uc) {
-		uc.findUserChar(ch, str);
-	}
-	
 	
 	
 	
