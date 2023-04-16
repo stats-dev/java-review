918ad9cdb87155cf9f0b105e04c1dfe911e61df1
stats-dev <hohun94@naver.com>
2023-04-16 19:17:18 +0900

    comparison between two integer numbers.

---
+++ JavaProgramming/src/chap100_coding_test/_04_comparison.java
@@ -0,0 +1,34 @@
+package chap100_coding_test;
+
+public class _04_comparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 11;
+		int num2 = 9;
+		
+		int answer = num1 == num2 ? 1 : -1;
+		
+		System.out.println(answer);
+		
+		
+	}
+
+}
+
+//class Solution {
+//    public int solution(int num1, int num2) {
+//        int answer = 0;
+//        
+//        // if(num1 == num2){
+//        //     answer = 1;
+//        // } else {
+//        //     answer = -1;
+//        // }
+//        
+//        answer = num1 == num2 ? 1 : -1;
+//        
+//        return answer;
+//    }
+//}
