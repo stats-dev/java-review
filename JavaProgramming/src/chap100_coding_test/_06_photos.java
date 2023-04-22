18e9a52662a6b43f59947f367a26111ed604e013
stats-dev <hohun94@naver.com>
2023-04-22 17:41:05 +0900

    java coding test solution 1

---
+++ JavaProgramming/src/chap100_coding_test/_06_photos.java
@@ -0,0 +1,50 @@
+package chap100_coding_test;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _06_photos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] name = {"may","kein","kain","radi"};
+		int[] year = {5,10,1,3};
+		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"},{"kon", "kain", "may", "coni"}};
+		int[] result = new int[photo.length];
+		int sum = 0;
+		//map으로 이름과 year 값을 연결해줍니다.
+		Map<String, Integer> photoMap = new HashMap<String, Integer>();
+		
+		
+		for(String[] p : photo) {
+			sum = 0;
+			
+			for(int i = 0; i < p.length; i++) {
+				if (p[i].equals("may")) {
+					sum += 5;
+				} else if(p[i].equals("kein")) {
+					sum += 10;
+				} else if(p[i].equals("kain")) {
+					sum += 1;
+				} else if(p[i].equals("radi")) {
+					sum += 3;
+				}
+			}
+			
+			photoMap.put(p.toString(), sum);
+
+		}
+		
+		System.out.println(photoMap.toString());
+		//entryset으로 잘 나오게 출력해보기.
+		
+		
+
+		
+//		if(photo[1])
+		
+		
+	}
+
+}
