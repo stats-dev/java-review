6d263f898d277d47d832ed084986c5b4e4773cf5
stats-dev <hohun94@naver.com>
2023-04-27 20:37:15 +0900

    JTextField and font size changed and adding button action with printing the text written on the button

--- JavaProgramming/src/chap99_projects/CalFrame_jPanel.java
+++ JavaProgramming/src/chap99_projects/CalFrame_jPanel.java
@@ -3,12 +3,15 @@ package chap99_projects;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -31,41 +34,86 @@ public class CalFrame_jPanel extends JFrame {
 		//메인 화면, 결과 출력창을 넣을 부분 추가
 		JPanel panel = new JPanel();
 		
+		//버튼 배열을 만들고, 채우기
+		JButton[] buttons = new JButton[16];
+		
 
 
 		
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 //		panel.add(new JLabel("숫자를 입력하세요 : "));
 		
-		//입력창을 생성한다. 정확히는 버튼 누르면 나오는 출력창이다.
-		container.add(new JTextField(), BorderLayout.NORTH);
+		//입출력창을 생성한다. 정확히는 버튼 누르면 나오는 출력창이다.
+		JTextField txtField = new JTextField();
+		//출력창의 크기를 조정한다.
+		txtField.setPreferredSize(new Dimension(100,50));
+		//창의 크기를 조정하고, 창 주위로 여백을 줍니다. 근데 이건 작동이 잘 안된다.
+//		txtField.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		//글자 크기를 조정합니다.
+		txtField.setFont(new Font("Arial", Font.PLAIN, 20));
+		
+		container.add(txtField, BorderLayout.NORTH);
 		
 		
 		//계산기는 4 * 4 이다. 그리고 여백을 10포인트씩 가로 세로 부여한다.
 		panel.setLayout(new GridLayout(4,4,10,10));
 		
 		for(int i=7; i<=9; i++) {
-			panel.add(new JButton(String.valueOf(i)));
+			buttons[i-7] = new JButton(String.valueOf(i));
+			panel.add(buttons[i-7]);
 		}
 		
-		add(new JButton(String.valueOf("+")));
+		buttons[3] = new JButton(String.valueOf("+"));
+		panel.add(buttons[3]);
 		
+		//4,5,6
 		for(int i=4; i<=6; i++) {
-			panel.add(new JButton(String.valueOf(i)));
+			buttons[i] = new JButton(String.valueOf(i));
+			panel.add(buttons[i]);
 		}
 		
-		panel.add(new JButton(String.valueOf("-")));
-		
+
+		//7,8,9번째
 		for(int i=1; i<=3; i++) {
-			panel.add(new JButton(String.valueOf(i)));
+			buttons[i+6] = new JButton(String.valueOf(i));
+			panel.add(buttons[i+6]);
 		}
 		
-		panel.add(new JButton(String.valueOf("x")));
-		panel.add(new JButton(String.valueOf(0)));
 		
-		panel.add(new JButton("00"));
-		panel.add(new JButton("="));
-		panel.add(new JButton("/"));
+		buttons[10] = new JButton(String.valueOf("-"));
+		buttons[11] = new JButton(String.valueOf("x"));
+		buttons[12] = new JButton(String.valueOf(0)); 
+		buttons[13] = new JButton(String.valueOf("00"));
+		buttons[14] = new JButton(String.valueOf("="));
+		buttons[15] = new JButton(String.valueOf("/"));
+		
+		panel.add(buttons[10]);
+		panel.add(buttons[11]);
+		panel.add(buttons[12]);
+		panel.add(buttons[13]);
+		panel.add(buttons[14]);
+		panel.add(buttons[15]);
+
+		
+		for(int i = 0; i < buttons.length; i++) {
+			JButton button = buttons[i];
+			button.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					txtField.setText(button.getText());
+					
+				}
+			});
+		}
+		
+		
+//		panel.add(new JButton(String.valueOf(0)));
+//		
+//		panel.add(new JButton("00"));
+//		panel.add(new JButton("="));
+//		panel.add(new JButton("/"));
 		
 		
 		//문제는 grid layout은 컴포넌트 크기가 고정된다고 한다.
