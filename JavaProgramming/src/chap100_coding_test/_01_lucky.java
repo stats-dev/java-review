c2f95738795d37be6836f91e999c6369e0d15020
stats-dev <>
2023-05-29 15:53:11 +0900

    git coding test updates

---
+++ JavaProgramming/src/chap100_coding_test/_01_lucky.java
@@ -0,0 +1,15 @@
+package chap100_coding_test;
+
+public class _01_lucky {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 123402;
+		
+		System.out.println(n);
+		
+		
+	}
+
+}
---
+++ JavaProgramming/src/chap100_coding_test/_01_lucky1.java
@@ -0,0 +1,17 @@
+package chap100_coding_test;
+
+//import java.util.Collection;
+
+public class _01_lucky1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 123402;
+		
+		
+		
+		
+		
+	}
+
+}
